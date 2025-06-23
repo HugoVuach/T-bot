11e310f36d2d8a85d58ddc7d2b3b9f43301dd4c4
Hugo VUACHEUX <ho.vhe@outlook.fr>
2025-06-23 11:02:13 +0200

    ajout de ligne de niveau au plus haut et au plus bas

--- ui/live_plot.py
+++ ui/live_plot.py
@@ -12,8 +12,20 @@ class LivePlotWidget(QWidget):
         self.plot_widget.setBackground('w')
         self.plot_widget.setTitle("Prix BTC moyen par seconde")
         self.plot_widget.setLabel('left', 'Prix moyen (USDT)')
-        # self.plot_widget.setLabel('bottom', 'Temps (s)')
         self.plot_widget.showGrid(x=True, y=True)
+
+        ### dernier plus haut et plus bas ###
+        self.high_line = pg.InfiniteLine(angle=0, pen=pg.mkPen('g', width=1.5, style=pg.QtCore.Qt.DashLine))
+        self.low_line = pg.InfiniteLine(angle=0, pen=pg.mkPen('r', width=1.5, style=pg.QtCore.Qt.DashLine))
+        self.plot_widget.addItem(self.high_line)
+        self.plot_widget.addItem(self.low_line)
+
+        self.high_label = pg.TextItem(color='g', anchor=(0, 1))
+        self.low_label = pg.TextItem(color='r', anchor=(0, 0))
+        self.plot_widget.addItem(self.high_label)
+        self.plot_widget.addItem(self.low_label)
+        ########################################
+
 
         self.curve = self.plot_widget.plot(pen='b')
 
@@ -30,9 +42,6 @@ class LivePlotWidget(QWidget):
 
             x_values = list(range(len(times)))
             self.curve.setData(x=x_values, y=prices)
-
-            ### On affiche l'heure à chaque seconde ###
-            #ticks = [(i, times[i]) for i in range(len(times))]
             
             ### On affiche un label toutes les 5 secondes ###
             tick_interval = 5
@@ -40,6 +49,21 @@ class LivePlotWidget(QWidget):
 
             axis = self.plot_widget.getAxis('bottom')
             axis.setTicks([ticks])  # une seule ligne de ticks
+
+            ### Affichage du plus haut / plus bas ###
+            high = max(prices)
+            low = min(prices)
+            self.high_line.setPos(high)
+            self.low_line.setPos(low)
+            # Positionner les labels à droite du dernier point visible
+            if x_values:
+                last_x = x_values[-1]
+                self.high_label.setText(f"↑ {high:.2f}")
+                self.low_label.setText(f"↓ {low:.2f}")
+                self.high_label.setPos(last_x, high)
+                self.low_label.setPos(last_x, low)
+
+            ###########################""
 
         except Exception as e:
             print(f"Erreur mise à jour graphique : {e}")
