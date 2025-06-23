a8c15f4f0a5251e7e2fe4acebb447960eb1acc03
Hugo VUACHEUX <ho.vhe@outlook.fr>
2025-06-23 10:46:12 +0200

    horodatage en h:m:s

--- ui/live_plot.py
+++ ui/live_plot.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget
 import pyqtgraph as pg
 from PyQt5.QtCore import pyqtSlot
+import datetime
 
 
 class LivePlotWidget(QWidget):
@@ -11,7 +12,7 @@ class LivePlotWidget(QWidget):
         self.plot_widget.setBackground('w')
         self.plot_widget.setTitle("Prix BTC moyen par seconde")
         self.plot_widget.setLabel('left', 'Prix moyen (USDT)')
-        self.plot_widget.setLabel('bottom', 'Temps (s)')
+        # self.plot_widget.setLabel('bottom', 'Temps (s)')
         self.plot_widget.showGrid(x=True, y=True)
 
         self.curve = self.plot_widget.plot(pen='b')
@@ -23,8 +24,21 @@ class LivePlotWidget(QWidget):
     @pyqtSlot(object)
     def update_plot(self, df):
         try:
-            x = list(range(len(df)))
-            y = df['mean_price'].values
-            self.curve.setData(x, y)
+        #### avec un horodatage 0 1 2 ect pas de heure en direct #########
+            #x = list(range(len(df)))
+            #y = df['mean_price'].values
+            #self.curve.setData(x, y)
+        ##############################################################
+            times = df['second'].tolist()  # colonne 'second' = texte genre "10:39:25"
+            prices = df['mean_price'].values
+
+            x_values = list(range(len(times)))
+            self.curve.setData(x=x_values, y=prices)
+
+            # Création d'un dictionnaire de labels horaires
+            ticks = [(i, times[i]) for i in range(len(times))]
+            axis = self.plot_widget.getAxis('bottom')
+            axis.setTicks([ticks])  # une seule ligne de ticks
+                  
         except Exception as e:
             print(f"Erreur mise à jour graphique : {e}")
--- ui/main_window.py
+++ ui/main_window.py
@@ -27,7 +27,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("BTC Live Dashboard")
-        self.resize(800, 600)
+        self.resize(1600, 1200)
 
         self.plot_widget = LivePlotWidget()
         container = QWidget()
